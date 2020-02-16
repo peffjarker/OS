9a49563ccad81ffc72924802c9d40df398a227cf
Jeffery Parker <36011518+peffjarker@users.noreply.github.com>
2020-02-16 17:28:49 -0500

    Update cpu_usage.cc

--- CPU_USAGE/cpu_usage.cc
+++ CPU_USAGE/cpu_usage.cc
@@ -1,1 +1,34 @@
 
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int cpu_use;
+  cin >> cpu_use;
+
+  int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y,
+      z, aa, ba, ca, da, ea, fa, ga, ha, ia, ja, ka, la, ma, na, oa, pa, qa, ra,
+      sa, ta, ua, va, wa, xa, ya, za, ab, bb, cb, db, eb, fb, gb, hb, ib, jb,
+      kb, lb, mb, nb, ob, pb, qb, rb, sb, tb, ub, vb, wb, xb, yb, zb, ac, bc,
+      cc, dc, ec, fc, gc, hc, ic, jc, kc, lc, mc, nc, oc, pc, qc, rc, sc, tc,
+      uc, vc;
+
+    switch (cpu_use) {
+    case 1:
+      a + a;
+      break;
+    case 2:
+      a *a *b *b;
+      break;
+    case 3:
+      a *a *b *b *c *c;
+      break;
+    case 4:
+      a *a *b *b *c *c *d *d;
+      break;
+    }
+  } while (1);
+
+  return 0;
+}
